e179b08df768569416ed57759fdf8b601f31fb13
Tobias Koch <tobias@tobijk.de>
2015-04-26 13:51:32 +0300

    whitespace fixes

--- demo/plasma/plasma.c
+++ demo/plasma/plasma.c
@@ -8,61 +8,61 @@
 
 int main(void)
 {
-	VBESURFACE *vbesurface_ptr;
-	FLATIMAGE  *flatimage_ptr;
+    VBESURFACE *vbesurface_ptr;
+    FLATIMAGE  *flatimage_ptr;
 
-	ulong x, y, *offscreen, center_x, center_y;
-	ulong angle;
+    ulong x, y, *offscreen, center_x, center_y;
+    ulong angle;
 
-	float sin_table[1024];
-	float cos_table[1024];
+    float sin_table[1024];
+    float cos_table[1024];
 
-	ulong *colortable;
-	ulong colorindex;
+    ulong *colortable;
+    ulong colorindex;
 
-	if((flatimage_ptr = (FLATIMAGE*)
-            loadPcxImage((unsigned char*) "palette.pcx", 32)) == 0) {
-		return 1;
-	} else {
-		colortable = (long unsigned int*) &flatimage_ptr->image;
-	}
+    if((flatimage_ptr = (FLATIMAGE*)
+        loadPcxImage((unsigned char*) "palette.pcx", 32)) == 0) {
+        return 1;
+    } else {
+        colortable = (long unsigned int*) &flatimage_ptr->image;
+    }
 
-	if((vbesurface_ptr = VBEinit(320, 240, 32)) == 0) {
-		puts("\nCould not initialize video.");
-		free(flatimage_ptr);
-		return 2;
-	} else {
-		offscreen = (long unsigned int*) vbesurface_ptr->offscreen_ptr;
-	}
+    if((vbesurface_ptr = VBEinit(320, 240, 32)) == 0) {
+        puts("\nCould not initialize video.");
+        free(flatimage_ptr);
+        return 2;
+    } else {
+        offscreen = (long unsigned int*) vbesurface_ptr->offscreen_ptr;
+    }
 
-	for(angle = 0; angle < 1024; angle++)
-		sin_table[angle] = sin((float)(PI / 512.0 * angle));
-	for(angle = 0; angle < 1024; angle++)
-		cos_table[angle] = cos((float)(PI / 512.0 * angle));
+    for(angle = 0; angle < 1024; angle++)
+        sin_table[angle] = sin((float)(PI / 512.0 * angle));
+    for(angle = 0; angle < 1024; angle++)
+        cos_table[angle] = cos((float)(PI / 512.0 * angle));
 
-	angle = 0;
+    angle = 0;
 
-        while(!kbhit()) {
+    while(!kbhit()) {
 
-            angle += 16;
+        angle += 16;
 
-            center_x = 160 + 100 * cos_table[angle % 1024];
-            center_y = 120 + 100 * sin_table[angle % 1024];
+        center_x = 160 + 100 * cos_table[angle % 1024];
+        center_y = 120 + 100 * sin_table[angle % 1024];
 
-            for(x = 0; x < 320; x++)
-                    for(y = 0; y < 240; y++) {
-                            colorindex = 128
-                                         + 63 * sin_table[(long)((hypot(center_y + (y-center_y/2), center_x + (x-center_x/2)) / 16) * (512.0 / PI)) % 1024]
-                                         + 63 * sin_table[(long)((x / (30 + 15 * cos(y / 240))) * (512.0 / PI)) % 1024] *
-                                         cos_table[(long)((y / (11 + 20 * sin(x / 320))) * (512.0 / PI)) % 1024] ;
+        for(x = 0; x < 320; x++)
+                for(y = 0; y < 240; y++) {
+                        colorindex = 128
+                                     + 63 * sin_table[(long)((hypot(center_y + (y-center_y/2), center_x + (x-center_x/2)) / 16) * (512.0 / PI)) % 1024]
+                                     + 63 * sin_table[(long)((x / (30 + 15 * cos(y / 240))) * (512.0 / PI)) % 1024] *
+                                     cos_table[(long)((y / (11 + 20 * sin(x / 320))) * (512.0 / PI)) % 1024] ;
 
-                            offscreen[y*320+x] = colortable[colorindex*32];
-                    }
+                        offscreen[y*320+x] = colortable[colorindex*32];
+                }
 
-            flipScreen();
-	}
+        flipScreen();
+    }
 
-	VBEshutdown();
-	free(flatimage_ptr);
-	return 0;
+    VBEshutdown();
+    free(flatimage_ptr);
+    return 0;
 }
--- src/pcx.h
+++ src/pcx.h
@@ -3,60 +3,60 @@
 
 // PCX header structure
 struct PcxHeader {
-	unsigned char  manufacturer;
-	unsigned char  pcx_version;
-	unsigned char  pcx_encoding;
-	unsigned char  pcx_bits_per_pixel;
-	/* window dimensions */
-	unsigned short pcx_xmin;
-	unsigned short pcx_ymin;
-	unsigned short pcx_xmax;
-	unsigned short pcx_ymax;
-	/* non vital info    */
-	unsigned short pcx_hor_ppi;
-	unsigned short pcx_ver_dpi;
-	unsigned char  pcx_color_map[48];
-	unsigned char  reserved;
-	unsigned char  pcx_num_planes;
-	unsigned short pcx_bytes_per_line;
-	unsigned short pcx_palette_info;
-	/* mandatory only for PB.IV and above */
-	unsigned short pcx_hor_size;
-	unsigned short pcx_ver_size;
-	unsigned char  filler[54];
+    unsigned char  manufacturer;
+    unsigned char  pcx_version;
+    unsigned char  pcx_encoding;
+    unsigned char  pcx_bits_per_pixel;
+    /* window dimensions */
+    unsigned short pcx_xmin;
+    unsigned short pcx_ymin;
+    unsigned short pcx_xmax;
+    unsigned short pcx_ymax;
+    /* non vital info    */
+    unsigned short pcx_hor_ppi;
+    unsigned short pcx_ver_dpi;
+    unsigned char  pcx_color_map[48];
+    unsigned char  reserved;
+    unsigned char  pcx_num_planes;
+    unsigned short pcx_bytes_per_line;
+    unsigned short pcx_palette_info;
+    /* mandatory only for PB.IV and above */
+    unsigned short pcx_hor_size;
+    unsigned short pcx_ver_size;
+    unsigned char  filler[54];
 } __attribute__ ((packed));
 
 typedef struct PcxHeader PCXHEADER;
 
 struct FlatImage_8Bit {
-	unsigned long     width;
-	unsigned long     height;
-	unsigned char     palette[256][3];
-	unsigned char     image;
+    unsigned long     width;
+    unsigned long     height;
+    unsigned char     palette[256][3];
+    unsigned char     image;
 } __attribute__ ((packed));
 
 typedef struct FlatImage_8Bit FLATIMAGE_8Bit;
 
 struct FlatImage_32Bit {
-	unsigned long     width;
-	unsigned long     height;
-	unsigned long     image;
+    unsigned long     width;
+    unsigned long     height;
+    unsigned long     image;
 } __attribute__ ((packed));
 
 typedef struct FlatImage_32Bit FLATIMAGE_32Bit;
 
 struct FlatImage_16Bit {
-	unsigned long     width;
-	unsigned long     height;
-	unsigned short    image;
+    unsigned long     width;
+    unsigned long     height;
+    unsigned short    image;
 } __attribute__ ((packed));
 
 typedef struct FlatImage_16Bit FLATIMAGE_16Bit;
 
 struct FlatImage {
-	unsigned long     width;
-	unsigned long     height;
-	unsigned char     image;
+    unsigned long     width;
+    unsigned long     height;
+    unsigned char     image;
 } __attribute__ ((packed));
 
 typedef struct FlatImage FLATIMAGE;
